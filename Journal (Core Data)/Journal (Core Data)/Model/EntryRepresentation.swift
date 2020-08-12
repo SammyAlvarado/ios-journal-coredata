79edadb17423331e57ce54c4ea7dec8c35ff5fb0
Sammy Alvarado <isammyalvarado@gmail.com>
2020-08-11 21:54:15 -0400

    FireBase and EntryRepresentation
    Added my EntryRepresentation and also created a firebase.

---
+++ Journal (Core Data)/Journal (Core Data)/Model/EntryRepresentation.swift
@@ -0,0 +1,33 @@
+//
+//  EntryRepresentation.swift
+//  Journal (Core Data)
+//
+//  Created by Sammy Alvarado on 8/11/20.
+//  Copyright © 2020 Sammy Alvarado. All rights reserved.
+//
+
+import Foundation
+
+struct EntryRepresentation: Codable {
+    var identifier: String
+    var bodyText: String
+    var title: String
+    var timestamp: Date
+    var mood: String
+}
+
+
+/*
+ self.identifier = identifier
+ self.bodyText = bodyText
+ self.title = title
+ self.timestamp = timestamp
+ self.mood = mood.rawValue
+
+ identifier: UUID = UUID(),
+ bodyText: String,
+ title: String,
+ timestamp: Date,
+ mood: Mood = .😐,
+
+ */
